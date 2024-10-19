ab8822b0872fe72eb81f44ee05dbf59d03b91820
Prabesh Bhandari <prabesh.chitwan@gmail.com>
2024-10-19 22:24:07 +0545

    first commit

---
+++ essentials/script.js
@@ -0,0 +1,11 @@
+const obj = {
+  firstName: "Prabesh",
+  secondName: "Bhandari",
+  social: {
+    facebook: "SystemPrabesh",
+    instagram: "prabesh.don27",
+  },
+};
+
+const { instagram } = obj.social;
+//yeti garepaxi harek thau ma instagram vanne keyword use garda hunxa, hoina vane ta harek choti obj.social.instagram use garna parne hunthyo
